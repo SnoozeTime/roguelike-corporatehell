7a41ff45b563563e1a82c815f9fec3e751cb81fa
Benoit Eudier <benoit.eudier@gmail.com>
2018-06-12 07:42:42 +0900

    Add walking animation for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private float speed = 5f;
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
 
 	// Update is called once per frame
 	void Update () {
@@ -27,6 +34,16 @@ public class PlayerController : MonoBehaviour {
 
         if (vertical < 0) {
             basePosition.y -= speed * Time.deltaTime;
+        }
+
+        if (horizontal != 0 || vertical != 0) {
+            animator.SetFloat("MoveX", horizontal);
+            animator.SetFloat("MoveY", vertical);
+            animator.SetFloat("LastMoveX", horizontal);
+            animator.SetFloat("LastMoveY", vertical);
+            animator.SetBool("PlayerMoving", true);
+        } else {
+            animator.SetBool("PlayerMoving", false);
         }
         transform.position = basePosition;
 	}
