dd16e46553261c31b3a68d0b8c680616cbc5c7db
Benoit Eudier <benoit.eudier@gmail.com>
2018-06-24 18:31:36 +0900

    Begin UI for player's health

---
+++ Assets/Scripts/ui/GuiElement.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+
+/*
+  How to load our prefab UI from the resource folder.
+ */
+public enum GuiElement {
+    [DescriptionAttribute("FullHeart")]
+    FULL_HEART,
+
+    [DescriptionAttribute("EmptyHeart")]
+    EMPTY_HEART,
+}
